982337ec2a24ee292a1faed586df4e85d59d45ea
Margus Martsepp <Margus Martsepp@MargusMartsepp.lan>
2010-12-06 04:57:55 +0200

    tests

---
+++ test/AllTests.java
@@ -0,0 +1,20 @@
+import junit.framework.JUnit4TestAdapter;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import entity.CompensationDateSpanTest;
+
+/**
+ * The class <code>AllTests</code> builds a suite that can be used to run all of
+ * the tests within its package as well as within any subpackages of its
+ * package.
+ * 
+ * @generatedBy CodePro at 6.12.10 1:07
+ * @author Margus Martsepp
+ * @version $Revision: 1.0 $
+ */
+@RunWith(Suite.class) @Suite.SuiteClasses({ CompensationDateSpanTest.class }) public class AllTests {
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(AllTests.class);
+	}
+}
